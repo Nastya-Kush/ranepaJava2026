a7e41e95182b4b094d462a41cbb35655b3a1c39e
Анастасия <nastya.kushnareva2006@gmail.com>
2026-05-04 15:19:51 +0300

    ПЗ1_Куш

--- src/main/java/ru/ranepa/HrmApplication.java
+++ src/main/java/ru/ranepa/HrmApplication.java
@@ -19,10 +19,10 @@ public class HrmApplication {
 
     //предопределённые объекты
     private static void addTestEmployees(EmployeeRepository repository) {
-        repository.save(new Employee(null, "Кушнарева Анастасия", "Директор",
+        repository.save(new Employee(null, "Кушнарева Анастасия Алексеевна", "Директор",
                 BigDecimal.valueOf(300000), LocalDate.of(2023, 6, 20)));
 
-        repository.save(new Employee(null, "Гуретская Дарья", "Визнес Аналитик",
+        repository.save(new Employee(null, "Гурецкая Дарья Денисовна", "Бизнес Аналитик",
                 BigDecimal.valueOf(120000), LocalDate.of(2024, 3, 6)));
     }
 }
